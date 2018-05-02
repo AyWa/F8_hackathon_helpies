cbf622f65cb454018b0d25f3f672767d56215874
marc hurabielle <marc.hurabielle@gmail.com>
2018-05-01 19:11:09 -0700

    change welcome message

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function processPostback(event) {
         name = bodyObj.first_name;
         greeting = "Hi " + name + ". ";
       }
-      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+      var message = greeting + "My name is Helpies Bot. We are matching volonteers and organizers";
       sendMessage(senderId, {text: message});
     });
   }
