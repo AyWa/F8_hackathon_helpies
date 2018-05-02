57eef9196b7652b0db2344fdebbdf65df2be813d
marc hurabielle <marc.hurabielle@gmail.com>
2018-05-02 03:04:47 -0700

    log picture

--- index.js
+++ index.js
@@ -86,7 +86,8 @@ function processPostback(event) {
         name = bodyObj.first_name;
         picture = bodyObj.picture;
         console.log("bodyObj", bodyObj);
-        greeting = "Hi " + name + ". ";
+        console.log("picture" + picture);
+        greeting = "Hi " + name + ". " + picture;
       }
       var message = greeting + "My name is Helpies Bot. We are matching volunteers and organizers";
       // create user in firebase:
