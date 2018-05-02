6504ad3ba14b539d0b29da9221c5534efc28da96
beckychan92 <beckychan92@gmail.com>
2018-05-02 02:07:12 -0700

    search

--- web/src/components/search.js
+++ web/src/components/search.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import {Input} from 'react-materialize'
+import {Input} from 'react-materialize';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+    this.inputChange = this.inputChange.bind(this);
+
+  }
+  inputChange(e){
+    this.setState({term: e.target.value});
+    // console.log('input', this.state.term)
+  }
+
   render() {
     return (
       <div>
-        <Input placeholder="Search..." s={6} label="q" />
+        <Input placeholder="Search..." onChange={this.inputChange} s={6} label="Search for volunteers!" />
       </div>
     );
   }
