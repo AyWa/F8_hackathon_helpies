7ba6353bb2ab5c47d22554a64a2960a57a59a619
beckychan92 <beckychan92@gmail.com>
2018-05-01 23:41:10 -0700

    search working

--- web/src/components/search.js
+++ web/src/components/search.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
-import {Input} from 'react-materialize'
+import { Input } from 'react-materialize';
+import _ from 'lodash'; 
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    };
+    this.inputChange = this.inputChange.bind(this);
+
+  }
+  inputChange(e){
+    this.setState({term: e.target.value});
+      _.debounce(meetupListData.forEach(meetup => {
+        if(element === term) {
+          console.log('tre');
+        } else {
+          console.log('fal')
+        }
+      }), 500);
+    }
+
+
+  }
+
+
+  
+
   render() {
     return (
       <div>
-        <Input placeholder="Search..." s={6} label="q" />
+        <Input placeholder="Search..." onChange={this.inputChange} s={6} label="q" />
       </div>
     );
   }
