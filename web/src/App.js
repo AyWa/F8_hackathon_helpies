9284f3af35e85cbd049270aab547b9733b091f5e
beckychan92 <beckychan92@gmail.com>
2018-05-02 02:28:22 -0700

    include more raw data

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import {Button, Icon, Navbar, NavItem, Row, Col, Card, CardTitle, Collection, CollectionItem} from 'react-materialize'
+import {Button, Icon, Navbar, NavItem, Row, Col, Card, CardTitle, Collection, CollectionItem} from 'react-materialize';
 
-import Search from './components/search.js'
+import Search from './components/search';
+import MeetupList from './components/meetupList';
 
 import logo from './logo.svg';
 import './App.css';
@@ -19,6 +20,18 @@ class App extends Component {
         }, {
           title: "F8 hackathon",
           img: "https://s3.ap-northeast-2.amazonaws.com/festa-temp/django-girls-images/django-girls-cover.png"
+        }, {
+          title: "American Red Cross",
+          img: "https://upload.wikimedia.org/wikipedia/en/7/7f/American_Red_Cross_logo.svg"
+        }, {
+          title: "Meals on Wheels",
+          img: "https://betterlivesleeds.files.wordpress.com/2016/11/mowlogo.jpg?w=640&h=320"
+        }, {
+          title: "Tutoring",
+          img: "https://uwaylc.org/get/files/image/galleries/Reader-Tutor-Mentor.png"
+        }, {
+          title: "Earth Day",
+          img: "https://liferaftgroup.org/wp-content/uploads/2015/04/volunteer.jpg"
         }
       ]
     }
@@ -27,12 +40,14 @@ class App extends Component {
   render() {
     const { meetupListData } = this.state;
     return (
+      // <div>hi</div>
       <div className="App">
         <Navbar brand='Helpies' right>
         <NavItem href='get-started.html'><Icon>search</Icon></NavItem>
         <NavItem href='get-started.html'><Icon>more_vert</Icon></NavItem>
       </Navbar>
       <Search />
+      <MeetupList />
       <Row>
         <Col s={7} className='grid-example'>
         <Card className='small'
